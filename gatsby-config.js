7e461608e628f3826afbe184ee72942939eb2223
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-08-02 17:40:43 +0530

    react-helmet

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    siteMetadata: {
-        siteUrl: 'https://www.yourdomain.tld',
-        title: "Blog Website Using Gatsby",
-        description: "First Blog Site Made Using Gatsby",
-        image: ''    
-    }
-}
